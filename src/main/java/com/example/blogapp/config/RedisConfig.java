61a44e6b20e2e7c423b2f9c0cc3dff58d7bcaafe
Devansh Kushwaha <ctnldevansh@gmail.com>
2025-07-26 22:16:12 +0530

    "Update Application propertirs to use profiles"

---
+++ src/main/java/com/example/blogapp/config/RedisConfig.java
@@ -0,0 +1,5 @@
+package com.example.blogapp.config;
+
+public class RedisConfig {
+    
+}
--- src/test/java/com/example/blogapp/BlogappApplicationTests.java
+++ src/test/java/com/example/blogapp/BlogappApplicationTests.java
@@ -2,17 +2,10 @@ package com.example.blogapp;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.net.ssl.SSLSocket;
-import javax.net.ssl.SSLSocketFactory;
-
 @SpringBootTest
 class BlogappApplicationTests {
 
-	public static void main(String[] args) throws Exception {
-		SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-		try (SSLSocket socket = (SSLSocket) factory.createSocket("ac-bgmwoep-shard-00-00.jnq2xho.mongodb.net", 27017)) {
-			socket.startHandshake();
-			System.out.println("SSL handshake successful!");
-		}
+	public static void main(String[] args) {
+		//Empty for now
 	}
 }
